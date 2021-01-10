048872a58c4f2e7f1f1d26e4bbbfa79e239037d4
hamidrezakamkari <hamidrezakamkari@gmail.com>
2021-01-10 22:13:57 +0330

    csv commands on local computer stored

--- Database/insert-rows.sql
+++ Database/insert-rows.sql
@@ -1,6 +1,55 @@
-insert into patientT
-values(1, 'hamid', 'kamkari', 20, 'MALE', 'student', null, 'Bachelors-Degree', null, null);
+copy PatientT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\patientt.csv'
+delimiter ','
+CSV header;
 
-insert into PatientPhonesT values(1, '09330347366');
+copy PatientPhonesT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\patientphonest.csv'
+delimiter ','
+CSV header;
 
-insert into filet values(1, '1999-01-08');
+copy FileT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\filet.csv'
+delimiter ','
+CSV header;
+
+copy PageT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\paget.csv'
+delimiter ','
+CSV header;
+
+copy MedicalImagePageT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\medicalimagepaget.csv'
+delimiter ','
+CSV header;
+
+copy WeeklyScheduleT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\weeklyschedulet.csv'
+delimiter ','
+CSV header;
+
+copy AvailableTimeSlotsT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\availabletimeslotst.csv'
+delimiter ','
+CSV header;
+
+copy OccupiedTimeSlotsT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\occupiedtimeslotst.csv'
+delimiter ','
+CSV header;
+
+copy AppointmentPageT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\appointmentpaget.csv'
+delimiter ','
+CSV header;
+
+copy PersonalInfoPageT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\personalinfopaget.csv'
+delimiter ','
+CSV header;
+
+copy ReferralOccupiedTimeSlotsT
+from 'G:\Projects\DB\DentistsOffice\Database\CSV\referraloccupiedtimeslotst.csv'
+delimiter ','
+CSV header;
+
