74cad4a24f47ee92b7944f6219119deb7dfc8a4b
kian <bakhtari.kian@gmail.com>
2021-02-01 18:04:36 +0330

    sumOfDebt added in SearchResults.

--- src/model/SearchResult.java
+++ src/model/SearchResult.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 
 public class SearchResult {
 
@@ -10,6 +12,9 @@ public class SearchResult {
     private ArrayList<String> lastNames = new ArrayList<>();
     private ArrayList<Integer> patientIds = new ArrayList<>();
     private ArrayList<Integer> debt = new ArrayList<>();
+
+    private ArrayList<Integer> unique_ids = new ArrayList<>();
+    private ArrayList<Integer> sum_debts = new ArrayList<>();
 
 
     public static SearchResult getInstance() {
@@ -55,5 +60,40 @@ public class SearchResult {
 
     public void setDebt(ArrayList<Integer> debt) {
         this.debt = debt;
+    }
+
+    public void sumDebts() {
+            HashMap<Integer, ArrayList<Integer>> debts = new HashMap<>();
+            for (int i = 0; i < patientIds.size(); i++) {
+                debts.putIfAbsent(patientIds.get(i), new ArrayList<Integer>());
+                debts.get(patientIds.get(i)).add(debt.get(i));
+            }
+            for (Integer i : debts.keySet()) {
+                unique_ids.add(i);
+            }
+            for (int i = 0; i < unique_ids.size(); i++) {
+                ArrayList<Integer> a = debts.get(unique_ids.get(i));
+                int sum = 0;
+                for (int j = 0; j < a.size(); j++) {
+                    sum += a.get(i);
+                }
+                sum_debts.add(sum);
+            }
+    }
+
+    public ArrayList<Integer> getUnique_ids() {
+        return unique_ids;
+    }
+
+    public void setUnique_ids(ArrayList<Integer> unique_ids) {
+        this.unique_ids = unique_ids;
+    }
+
+    public ArrayList<Integer> getSum_debts() {
+        return sum_debts;
+    }
+
+    public void setSum_debts(ArrayList<Integer> sum_debts) {
+        this.sum_debts = sum_debts;
     }
 }
