8e87df073b54997ca5d017f0d43bbb04e1939cae
hamidrezakamkari <hamidrezakamkari@gmail.com>
2021-02-02 18:02:38 +0330

    some bugs removed

--- src/view/PopUpCreater.java
+++ src/view/PopUpCreater.java
@@ -1,7 +1,19 @@
 package view;
 
+import controller.graphics.FXMLLoadersCommunicator;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.popups.PopupsGUI;
+
 public class PopUpCreater {
     public static void createSQLErrorPopUp(String errorMessage) {
-
+        Parent root = null;
+        FxmlFileLoader object = new FxmlFileLoader();
+        root = object.getPage("PopUpSQLError", PopupsGUI.class);
+        ((PopUpSQLError) FXMLLoadersCommunicator.getLoader("PopUpSQLError").getController()).setText(errorMessage);
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
